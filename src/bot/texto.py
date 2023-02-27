e43c64c5f06c6341572234980ffdc1822c5c272a
Michael Douglas <74698250+douglasmd3@users.noreply.github.com>
2023-02-26 21:07:16 -0300

    Create texto.py

---
+++ src/bot/texto.py
@@ -0,0 +1,227 @@
+HOME = "HOME"
+ESTRUTURA_ADMINISTRATIVA = "ESTRUTURA_ADMINISTRATIVA"
+VOLTAR_FAQ_SEAC = "VOLTAR_FAQ_SEAC"
+SEAC_SGA = "SEAC/SGA"
+CONTATO_SEAC = "CONTATO_SEAC"
+COEX_SGA = "COEX/SGA"
+CONTATO_COEX = "CONTATO_COEX"
+FAQ_SEAC = "FAQ_SEAC"
+FAQ_COEX = "FAQ_COEX"
+SUGERIR = "SUGERIR"
+AVALIAR = "AVALIAR"
+BOM = "GOOD"
+RUIM = "BAD"
+NORMAL = "NORMAL"
+start_texto = "Para prosseguir, selecione uma opção disponível que poderá lhe ajudar no seu atendimento 👇"
+
+
+FAQSEAC1 = "faq_seac1"
+FAQSEAC2 = "faq_seac2"
+FAQSEAC3 = "faq_seac3"
+FAQSEAC4 = "faq_seac4"
+FAQSEAC5 = "faq_seac5"
+FAQSEAC6 = "faq_seac6"
+FAQSEAC7 = "faq_seac7"
+FAQSEAC8 = "faq_seac8"
+FAQSEAC9 = "faq_seac9"
+FAQSEAC_10 = "faq_seac_10"
+txt_seac = """A secretaria Acadêmica | SEAC é o setor responsável pelo controle, verificação, registro e arquivamento da documentação da vida acadêmica do aluno, desde seu ingresso na Instituição, até a conclusão e/ou expedição do diploma.\n\nA missão da Secretaria Acadêmica é realizar com presteza e eficácia todos os procedimentos que envolvem o controle e o registro acadêmico, assim como orientar os alunos para os mesmos."""
+
+seac_contato = """\n
+Responsáveis:
+Renato Silva de Oliveira (Secretário acadêmico)
+Anderson de Brito Rodrigues
+Maria Vilanir Gonçalves Duarte da Silva
+Rafael da Silva Araújo\n
+Informação para contato:
+Telefone: (84) 4005-4111
+Ramal 7406 / 7436
+E-mail: seac.sga@ifrn.edu.br
+Canal Telegram: https://t.me/secretariaacademicaifrnsga
+"""
+
+txt_coex = """A Coordenação de Extensão (COEX) tem como objetivo central garantir a interação
+entre o IFRN e a comunidade externa.\n"""
+txt_coex += """\nCom isso, busca garantir a divulgação, o desenvolvimento de novas tecnologias e a 
+manutenção da relação com o mundo do trabalho para que desta forma seja efetiva a qualidade da formação 
+dos nossos estudantes."""
+
+coex_contato = """\n
+Responsáveis:
+Fernando de Oliveira Freire (Coordenador)\n
+Informação para contato:
+Telefone: (84) 4005-4111 - Ramal 7410
+E-mail: 
+coex.sga@ifrn.edu.br 
+extensão.sga@gmail.com
+Canal Telegram: https://t.me/coordenacaodeextensaoifrnsga
+Grupo WhatsApp: https://chat.whatsapp.com/LNw9BvA5qsV0H3QOeypzDK
+Instagram: https://www.instagram.com/coex.sga/
+Link Coex: https://linktr.ee/coex.sga
+"""
+
+FAQ = """
+\nOlá, caro(a) requerente 🤗 há alguma dúvida para esclarecer ❓ Aqui segue algumas das perguntas frequentes do setor selecionado para que você fique bem informado das principais questões tratadas por este setor.\n
+veja e selecione aquela que melhor atende sua expectativa. Espero que isso lhe ajude em sua busca.
+Caso não encontre o que procura, minhas recomendações são:
+⚠ 1 - Entre em contato com o setor 🏢
+⚠ 2 - Entre em contato com o gerente deste sistema para /sugerir alguma implementação 💬 @marcelo_d_melo @michael_moreira
+⚠ 3 - Busque atendimento presencial com o setor nos horários de funcionamento ⏱ 
+"""
+
+txt_comum = """Link SUAP: https://suap.ifrn.edu.br/accounts/login/?next=/
+Link Requerimento Padrão: https://portal.ifrn.edu.br/campus/saogoncalo/arquivos/formularios-seac/requerimento-padrao/
+Com o tal requerimento é possível solicitar:
+- Matrículas;
+- Cancelamento de matrícula;
+- Renovação de matrícula (semestral ou anual);
+- Trancamento de matrícula ou disciplinas;
+- Adequação de horários / Estudo individualizado;
+- Atendimento domiciliar;
+- Dispensa de atividades;
+- Justificativa de falta;
+- Mudança de curso / turma / turno;
+- Reposição de atividades;
+- Revisão de faltas / notas / situação;
+- Transferência """
+
+txt_faq_seac1 = """Como justificar faltas❓ ou\nComo solicitar reposição de atividades❓\n
+A solicitação deverá ser realizada através de chamado no SUAP, anexando o requerimento juntamente com os documentos comprobatórios.
+"""
+
+txt_faq_seac2 = f"""Como solicitar mudança de tuno ou turma❓\n
+A solicitação deverá ser realizada através de chamado no SUAP, anexando o requerimento juntamente com justificativa irrefutável da necessidade de mudança de turno ou turma.
+Para menores de 18 anos o requerimento deverá ser assinado pelo responsável juntamente com a cópia do RG (Frente e Verso) do responsável.
+{txt_comum}
+"""
+
+txt_faq_seac3 = f"""Como solicitar o aproveitamento de estudos❓\n
+A solicitação deverá ser realizada através de chamado no SUAP, anexando o requerimento juntamente com os documentos comprobatórios.\n
+O requerimento para aproveitamento de estudos deverá ser acompanhado dos seguintes documentos:\n
+⚠ I.	Histórico acadêmico;
+⚠ II.	Programas de disciplinas cursadas, objeto da solicitação; e
+⚠ III.	Documento que comprove a autorização de funcionamento ou o reconhecimento do curso de origem.
+
+Art. 269. Poderão ser objeto de aproveitamento de estudos as disciplinas:\n
+⚠ a)  cujos conteúdos e cargas horárias coincidirem em, no mínimo, 70% (setenta por cento) com os programas das disciplinas do respectivo curso oferecido pelo IFRN;
+⚠ b)  cursadas com aprovação em outros cursos do mesmo nível de ensino ou outros cursos de nível posterior, independentemente da nota final obtida, excetuando-se os cursos técnicos de nível médio nos quais somente poderá ser concedido o aproveitamento de disciplinas cursadas em outro curso técnico de nível médio;
+⚠ c)  cursadas antes do ingresso do estudante no IFRN;
+⚠ d)  cursada num prazo máximo de 5 (cinco) anos, decorridos entre o final do período em que a disciplina foi cursada e a data de requerimento do aproveitamento de estudos; e
+⚠ e)  cujas disciplinas pré-requisitos, quando houver, tiverem sido integralizadas.
+
+⚠ § 1º. A equivalência de estudos poderá ser contabilizada a partir de estudos realizados em uma disciplina ou em duas ou mais disciplinas que se complementam no sentido de integralizar uma disciplina do curso.
+⚠ § 2º. A análise de equivalência entre matrizes curriculares será realizada pelo Coordenador de Curso, que encaminhará o processo para análise de equivalência entre programas de disciplinas.
+⚠ § 3º. A análise de equivalência entre programas de disciplinas será realizada por pelo menos um docente especialista da disciplina objeto do aproveitamento, que emitirá parecer conclusivo sobre o pleito.
+⚠ § 4º. A avaliação da correspondência de estudos deverá recair sobre os conteúdos que integram os programas das disciplinas apresentadas e não sobre a denominação das disciplinas cursadas. § 5º. Será registrada no histórico acadêmico do estudante a média aritmética ponderada da(s) disciplina(s) aproveitadas, tendo como peso a carga horária da(s) disciplina(s) correlata(s).
+
+Art. 270. É vedado o aproveitamento de estudos de disciplinas em que o requerente haja sido reprovado no IFRN.  Organização Didática do IFRN. IFRN, 2012.
+Art. 271. Com vistas ao aproveitamento de estudos, os(as) estudantes de nacionalidade estrangeira ou brasileiros(as) com estudos realizados no exterior deverão apresentar documentação legalizada por via diplomática e com equivalência concedida pelo respectivo sistema de ensino.\n
+{txt_comum}
+"""
+
+txt_faq_seac4 = """Como requerer a certificação de conhecimentos❓\n
+A inscrição para a certificação de conhecimentos deverá ser efetuada através de chamado no SUAP, anexando o requerimento com a enumeração das disciplinas requeridas.\n
+Art. 272. O processo de certificação de conhecimentos consistirá em uma avaliação teórica ou teórico-prática, conforme as características da disciplina, com calendário de provas a ser divulgado pela Diretoria Acadêmica ofertante.\n
+Art. 273. A certificação de conhecimentos será realizada por uma banca examinadora designada pelo respectivo Diretor Acadêmico, ouvido o Coordenador de Curso, e constituída por um membro da equipe técnico-pedagógica e, no mínimo, dois docentes especialistas da(s) disciplina(s) em que o estudante será avaliado, cabendo a essa comissão emitir parecer conclusivo sobre o pleito.\n
+⚠ Parágrafo único. Será dispensado de cursar uma disciplina o estudante que alcançar aproveitamento igual ou superior a 60 (sessenta) nessa avaliação, sendo registrado no seu histórico acadêmico o resultado obtido no processo.\n
+Art. 274. A inscrição para a certificação de conhecimentos deverá ser efetuada através de requerimento ao respectivo Diretor Acadêmico, com a enumeração das disciplinas requeridas.\n 
+⚠ § 1º. O número máximo de requerimentos para realização de certificação de conhecimentos não deverá exceder 4 (quatro) avaliações por estudante em cada período letivo.\n
+⚠ § 2º. Para cada disciplina do curso, será permitido ao estudante requerer a certificação de conhecimentos uma única vez.\n 
+Art. 275. Em caso de ausência a qualquer avaliação de certificação de conhecimentos, esta ficará automaticamente cancelada, não cabendo recurso.\n 
+Art. 276. É vedada a certificação de conhecimentos de disciplinas em que o requerente tenha sido reprovado no IFRN.\n 
+Art. 277. Para os cursos técnicos na forma integrada, é vedada a certificação de conhecimentos de disciplinas referentes aos núcleos fundamental e estruturante.\n 
+"""
+
+txt_faq_seac5 = f"""Como solicitar emissão de Diploma❓\n
+A solicitação deverá ser realizada através de chamado no SUAP, anexando cópia do RG, CPF, Certidão de Nascimento/ Casamento e Nada-Consta (O documento é feito virtualmente pela SEAC).
+O prazo de atendimento é de até 30 dias.\n
+{txt_comum}\n
+clique no link para ver um vídeo tutorial:
+https://www.youtube.com/watch?v=ABqkjmbtLp8
+
+"""
+
+txt_faq_seac6 = f"""Como solicitar transferências❓\n
+⚠ I.	TRANSFERÊNCIA COMPULSÓRIA:\n
+Art. 201. Nos casos de transferência de servidor público civil ou militar, removido ex-oficio e de seus dependentes – quando for caracterizada a interrupção de estudos, a matrícula será concedida independentemente de vaga e de prazos estabelecidos, caracterizando a TRANSFERÊNCIA COMPULSÓRIA.\n
+⚠ II.	TRANSFERÊNCIA POR DEFERIMENTO:\n
+Art. 202. Em caso de mudança de domicílio de estudantes trabalhadores ou dos seus responsáveis, para estudante regularmente matriculado no IFRN, poderá ser concedida a TRANSFERÊNCIA POR DEFERIMENTO, após análise de processo pela Pró-reitora de Ensino, formulado pelo estudante, constando parecer da pedagogia e do serviço social do campus ao qual o estudante está vinculado, comprovando a impossibilidade da permanência.\n
+{txt_comum}
+"""
+
+txt_faq_seac7 = f"""Como fazer a renovação de matrícula❓\n
+A renovação de matrícula para cada período letivo deverá ser efetuada, obrigatoriamente, em data prevista no calendário acadêmico do campus de vinculação do estudante, mediante preenchimento de formulário próprio no SUAP.
+Necessitarão fazer a renovação de matrícula todos os estudantes regularmente matriculados, inclusive aqueles com matrícula trancada e em realização de prática profissional.\n
+Art. 209. O estudante com direito à renovação de matrícula que deixar de efetuá-la dentro dos prazos previstos deverá justificar o fato à respectiva Diretoria Acadêmica em até 20 (vinte) dias corridos. Após a data final estabelecida, será considerado desistente e terá sua matrícula cancelada por evasão.
+⚠ Parágrafo único. O processo de evasão deverá ser precedido de apuração, em que será dado ao estudante o direito à ampla defesa.\n 
+{txt_comum}
+"""
+
+txt_faq_seac8 = """Como realizar inscrição em disciplina❓\n
+Art. 210. Para os estudantes dos cursos com sistema de crédito, deverá ser realizada, semestralmente, no período especificado no calendário acadêmico do campus de vinculação do estudante, a Inscrição em Disciplinas.
+Parágrafo Único. Entende-se por Inscrição em Disciplinas a seleção formal das disciplinas a serem cursadas no período letivo subsequente.\n
+Art. 211. No semestre de ingresso no curso, o estudante deverá cursar todas as disciplinas constantes na matriz curricular, compatíveis com o seu período de referência.\n
+Art. 212. Para a matrícula por disciplinas, dentre as que estão sendo ofertadas no período letivo:
+⚠ I.	o estudante deverá cursar, em cada período letivo, no mínimo, 3 (três) disciplinas e, no máximo, o número de disciplinas do período de referência mais 2 (duas) disciplinas; e 
+⚠ II.	o estudante poderá cursar disciplinas de semestres distintos da matriz curricular, desde que sejam até 2 (dois) períodos subsequentes ao período de referência.
+Parágrafo único. Caso o estudante só esteja apto a cursar menos de três disciplinas, deverá ser realizada a matrícula em todas as disciplinas possíveis.\n 
+Art. 213. O estudante que não efetuar a inscrição em disciplinas será automaticamente matriculado nas disciplinas do período compatível com seu período de referência, não cabendo recurso.\n
+Art. 214. A seleção para disciplinas em que haja mais candidatos que vagas será realizada considerando a seguinte ordem: 
+⚠ I.	maior compatibilidade com o período da disciplina, caracterizado pelo período de referência do estudante e da disciplina; e 
+⚠ II. classificação decrescente do Índice de Rendimento Acadêmico (IRA).
+"""
+
+txt_faq_seac9 = f"""Como solicitar trancamento de matrícula❓\n
+O trancamento de matrícula poderá ocorrer de forma compulsória ou voluntária.\n
+Entende-se por trancamento de matrícula compulsório aquele em que o estudante necessite interromper os estudos nos seguintes casos, devidamente comprovados e atestados:
+⚠ convocação para o serviço militar obrigatório (ao completar 18 anos); 
+⚠ tratamento prolongado de saúde pessoal ou de familiares em primeiro grau, quando não couber o atendimento domiciliar especial; Organização Didática do IFRN.
+⚠ gravidez de alto risco ou problemas pós-parto; \nIV. intercâmbio educacional promovido pelo IFRN.\n
+Entende-se por trancamento de matrícula voluntário aquele em que o estudante faz a opção pela interrupção dos estudos.\n 
+⚠ A solicitação de trancamento de matrícula deverá ser feita mediante requerimento à Diretoria Acadêmica, pelo próprio estudante, quando maior de idade, ou por seu representante legal, quando menor de idade. O trancamento de matrícula só terá validade de 1 (um) período letivo.\n
+⚠ O trancamento de matrícula compulsório pode ser requerido em qualquer época do período letivo e não será computado para efeito de contagem de tempo máximo para integralização curricular.
+⚠ Nos casos em que, por reprovação ou trancamento de matrícula do estudante, não houver oferta de disciplinas pela Diretoria Acadêmica ao qual o estudante está vinculado, deverá ser realizada MATRÍCULA COM VÍNCULO.
+⚠ Em caso de matrícula com vínculo, o estudante deverá ser orientado pelo Coordenador do Curso para o desenvolvimento de atividades acadêmicas extracurriculares durante o período equivalente.
+⚠ O período letivo de permanência em matrícula com vínculo não será computado para efeito de contagem de tempo máximo para integralização curricular.
+⚠ O trancamento voluntário somente será autorizado após a integralização de todos os componentes curriculares do primeiro período do curso. 
+⚠ Para os estudantes com admissão por reingresso e transferência, o trancamento voluntário só poderá ser concedido quando for integralizado o período em que foi posicionado após a realização do aproveitamento dos estudos.
+⚠ O trancamento de matrícula voluntário deverá ser solicitado no período previsto no calendário acadêmico do campus de vinculação do estudante. 
+⚠ O estudante só poderá trancar matrícula, na forma voluntária, até 2 (duas) vezes durante todo o curso, e o tempo de trancamento será contabilizado para efeito de cálculo do prazo máximo para integralização curricular.
+⚠ Ao retomar às atividades acadêmicas, o estudante retomará o período letivo interrompido por ocasião do trancamento.
+⚠ Nos cursos com regime de crédito, o estudante fará o procedimento de inscrição em disciplinas.
+⚠ Nos cursos com regime seriado ou modular, o estudante deverá ser matriculado em todas as disciplinas da série ou módulo.
+⚠ Os estudantes com matrícula trancada cujo curso venha a sofrer mudanças no currículo deverão fazer as adaptações necessárias à nova situação.\n
+⚠ {txt_comum}
+"""
+
+txt_faq_seac_10 = """Como solicitar o Cancelamento de Disciplina ❓\n
+Art. 222. Será permitido o cancelamento de disciplinas para os estudantes dos cursos com regime de crédito, respeitado o prazo estabelecido no calendário acadêmico do campus de vinculação do estudante. 
+⚠ Parágrafo Único. Não será permitido o cancelamento de disciplinas para os cursos com regime seriado.\n
+Art. 223. O cancelamento de disciplinas não será concedido para disciplinas constantes do primeiro período na matriz curricular do curso.\n 
+Art. 224. O cancelamento de disciplina implica, obrigatoriamente, o cancelamento do projeto integrador vinculado.\n
+Art. 225. Cada disciplina poderá ser cancelada apenas uma vez. Para requerer o cancelamento de matrícula em disciplina, o estudante deverá realizar as seguintes etapas:
+⚠ 1) Fazer login por meio do endereço suap.ifrn.edu.br;
+⚠ 2) No menu clique em "ENSINO", e em "Dados do Aluno";
+⚠ 3) Clique na aba "Requerimentos". Depois clique no botão "Adicionar Requerimento";
+⚠ 4) Escolha "Cancelamento de Disciplina" no campo tipo do requerimento. Insira uma justificativa no campo "Descrição/Justificativa". Clique no botão "Salvar"; 
+⚠ 5) Caso tenha menos de 18 anos, anexe a imagem do RG do responsável legal. \nPronto! O requerimento foi criado. Assim que os servidores da secretaria Acadêmica abrirem o requerimento, a solicitação será avaliada e posteriormente será realizado o cancelamento da disciplina.
+"""
+
+txt_avaliar = """Olá, eu gostaria de sua avaliação. considere - me um chatbot inicial para futuras implementações  aos  outros setores.\n 
+No momento consigo fazer algo ali, outro  aqui; me endentes?\n 
+Sabendo disto, o chatbot pode ser útil para você na prestação de atendimentos e interação acadêmica.
+
+Qual a sua Avaliação:"""
+
+txt_sugestao = """Faça a sua sugestão ao nosso bot"""
+
+txt_confirmar_sugestao="""Você deseja confirmar a sua sugestão 👇"""
+
+txt_sugestao_agradecimento = "Obrigado pela sua sugestão"
+
+txt_sugestao_cancelada = """Ok, sua sugestão foi cancelada. Fica para uma próxima ne? 
+Sua sugestão será sempre bem-vinda para melhor lhe atender. Até logo."""
+
+txt_avaliar_agradecimento = """Agradeço por sua avaliação.\n
+Esse dado é um apoio importante, pois me preocupo em prestar um bom atendimento e sanar suas necessidades.\n
+Por isso sua colaboração é fundamental, utilize [ /sugerir ] e colabore em meu upgrade, desta forma poderei melhor lhe atender nas futuras atualizações."""
